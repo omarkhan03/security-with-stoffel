f52029737cec56c9d7c55162ab5645da454d7351
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-03-31 17:13:24 -0600

    Formatted code better

--- script.js
+++ script.js
@@ -4,22 +4,52 @@ const stoffel = document.getElementById("stoffel");
 const speech = document.getElementById("speech");
 
 const sequence = [
-    "Hi! I'm Stoffel the red panda!",
-    "I'm here to teach you about the security triad!"
+    {
+        speech: "Hi! I'm Stoffel the red panda!",
+        image: "images/teach1.jpeg"
+    },
+    {
+        speech: "I'm here to teach you about the security triad!",
+        image: "images/teach.jpeg"
+    },
+    {
+        speech: "There are three main principles of security: confidentiality, integrity, and availability.",
+        image: "images/smile.jpeg"
+    }
 ]
+
+let index = 0;
 
 back.style.display = 'none';
 
 next.addEventListener("click", () => {
     back.style.display = '';
     next.style.display = 'none';
-    stoffel.src="images/teach.jpeg"
-    speech.innerHTML = sequence[1];
+    index++;
+    updateStoffel();
+    updateUI();
 });
 
 back.addEventListener("click", () => {
     back.style.display = 'none';
     next.style.display = '';
-    stoffel.src="images/teach1.jpeg"
-    speech.innerHTML = sequence[0];
+    index--;
+    updateStoffel();
+    updateUI();
 })
+
+function updateStoffel() {
+    stoffel.src= sequence[index].image;
+    speech.innerHTML = sequence[index].speech;
+}
+
+function updateUI() {
+    if (index == 0) {
+        back.style.display = 'none';
+    } else if (index == sequence.length - 1) {
+        next.style.display = 'none';
+    } else {
+        back.style.display = '';
+        next.style.display = '';
+    }
+}
