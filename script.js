e0bb1b932677e09cbf48e18f484af585da619d24
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-04-06 14:42:29 -0600

    Improved navbar

--- script.js
+++ script.js
@@ -447,3 +447,25 @@ a4.addEventListener("click", () => {
     updateStoffel();
     updateUI();
 })
+
+
+// open modal by id
+function openModal(id) {
+    document.getElementById(id).classList.add('open');
+    document.body.classList.add('jw-modal-open');
+}
+
+// close currently open modal
+function closeModal() {
+    document.querySelector('.jw-modal.open').classList.remove('open');
+    document.body.classList.remove('jw-modal-open');
+}
+
+window.addEventListener('load', function() {
+    // close modals on background click
+    document.addEventListener('click', event => {
+        if (event.target.classList.contains('jw-modal')) {
+            closeModal();
+        }
+    });
+});
